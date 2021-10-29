8ee453b33d267d48d18b984c0375d73f47f03953
Pavithra <Pavithra@LAPTOP-6DSKMKT1>
2021-10-29 20:14:36 +0530

    Committing 21st task.

---
+++ CoreJava/MyResource/Calci.js
@@ -0,0 +1,19 @@
+var add = function(num1, num2) {
+	print("SUM : " + (num1 + num2));
+}
+
+var sub = function(num1, num2) {
+	print("DIFFERENCE : " + (num1 - num2));
+}
+
+var multi = function(num1, num2) {
+	print("PRODUCT : " + (num1 * num2));
+}
+
+var div = function(num1, num2) {
+	print("QUOTIENT : " + (num1 / num2));
+}
+
+var msg=function(){
+print("SUCCESSFULLY PRINTED!!!");
+}
---
+++ CoreJava/src/Session/NashornCalculate.java
@@ -0,0 +1,42 @@
+package Session;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.script.Bindings;
+import javax.script.Invocable;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class NashornCalculate {
+
+	public static void main(String[] args)
+			throws ScriptException, NoSuchMethodException, NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+
+		Bindings bind = engine.getBindings(ScriptContext.ENGINE_SCOPE);
+		engine.eval(new FileReader("MyResource/Calci.js"));
+
+		//Taking the two numbers from the User
+		System.out.println("ENTER THE TWO NUMBERS : ");
+		int num1 = Integer.parseInt(br.readLine());
+		int num2 = Integer.parseInt(br.readLine());
+
+		Invocable invocable = (Invocable) engine;
+		invocable.invokeFunction("add", num1, num2);
+		invocable.invokeFunction("sub", num1, num2);
+		invocable.invokeFunction("multi", num1, num2);
+		if (num2 == 0)
+			invocable.invokeFunction("div", num1, 1); // Default procedure
+		else
+			invocable.invokeFunction("div", num1, num2);
+		invocable.invokeFunction("msg");
+	}
+
+}
